336d3bdc1cec683874b0cd7e288670a9538d639f
Victor Bianchi <vbianchi@protonmail.com>
2022-02-17 07:48:33 -0500

    Header and footer markup+styling, index page first content

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,7 +2,9 @@
   <nav>
     <ul>
       <li>
-        <a href="/">Victor Bianchi</a>
+        <a href="/">
+          <span aria-hidden="true">&lt;</span>Victor&nbsp;Bianchi<span aria-hidden="true">&gt;</span>
+        </a>
       </li>
       <li>
         <a href="/blog">Blog</a>
@@ -28,7 +30,23 @@
     gap: 1rem;
   }
 
+  a {
+    font-size: 1.125rem;
+    font-weight: bold;
+    color: var(--color-foreground);
+    padding: .5rem 0;
+    text-decoration: none;
+    transition: color .1s ease-in;
+  }
+  
+  a:hover, a:focus-visible {
+    color: var(--color-accent-primary);
+  }
+
   li:first-child {
+    font-family: var(--font-family-monospace);
+    text-transform: lowercase;
     margin-right: auto;
   }
+
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,22 @@
-<h1>Hello world</h1>
+<h1>Hi, I'm Victor<span>👋</span></h1>
 
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto fuga animi ducimus.</p>
+<p>I'm a software developer, teacher, and cybersecurity student based in Montreal 🇨🇦</p>
+
+<p>I can't stop learning new stuff and love to share what I have learned. In this website you can find my résumé, my personal projects, and stories about how I created them.</p>
+
+<style>
+  h1 {
+    font-size: 4rem;
+  }
+
+  h1 > span {
+    color: initial;
+    margin-left: .5rem;
+  }
+
+  p:first-of-type {
+    font-size: 2rem;
+    line-height: 1.2;
+    margin-bottom: 2rem;
+  }
+</style>
