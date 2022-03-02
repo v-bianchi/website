482aac2c4d599c1d8cbd5f95ae4bda6f453ac3dc
Victor Bianchi <vbianchi@protonmail.com>
2022-03-02 14:08:11 -0500

    Add Callout component for blog posts

---
+++ src/lib/components/Callout.svelte
@@ -0,0 +1,16 @@
+<div>
+  <slot />
+</div>
+
+<style>
+  div {
+    background-color: var(--color-background-alt);
+    font-size: 1.2rem;
+    line-height: 1.4;
+    padding: var(--spacer-m);
+  }
+  
+  div:not(:last-child) {
+    margin-bottom: var(--spacer-m);
+  }
+</style>
